4badd4c0b15161ce0595e1bec72ac285a72b3773
KokuboHayate <k024g1005@g.neec.ac.jp>
2025-04-11 16:21:36 +0900

    コメントアウトの追加

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+//一般的な賃金体系の計算関数
 int Normal(int time) {
 	return time * 1072;
 }
 
+//再帰的な賃金体系の計算関数
 int Recursive(int time,int salary) {
 	if (time <= 1) {
 		return salary;
@@ -14,11 +16,14 @@ int Recursive(int time,int salary) {
 
 int main() 
 {
+	//労働時間を入れるの変数
 	int workinghours;
 
+	//労働時間を入力させる
 	printf("何時間働きましたか？->");
 	scanf_s("%d", &workinghours);
 
+	//それぞれの体系における賃金の表示
 	printf("一般的な賃金体系の場合:%d円\n", Normal(workinghours));
 	printf("再帰的な賃金体系の場合:%d円\n", Recursive(workinghours,100));
 
