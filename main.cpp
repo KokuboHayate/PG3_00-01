62269a9fd8348662ef62057c527117eb4809517e
KokuboHayate <k024g1005@g.neec.ac.jp>
2025-04-11 15:15:57 +0900

    関数MInの追加

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
 template <typename Type>
-Type add(Type a, Type b) {
-	return static_cast<Type>(a + b);
+Type Min(Type num1, Type num2) {
+	if (num1 < num2) {
+		return static_cast<Type>(num1);
+	} else {
+		return static_cast<Type>(num2);
+	}
+}
+
+template <>
+char Min<char>(char num1, char num2) {
+	printf("数字以外は代入できません");
+	return 0;
 }
 
 int main() 
 {
-	printf("%d\n", add<int>(114, 514));
-	printf("%f\n", add<float>(11.4f, 51.4f));
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(11.4f, 51.4f));
+	printf("%lf\n", Min<double>(11.4, 51.4));
+	//printf("%s\n", Min<char>(114, 514));
 
 	return 0;
 }
