be9b8a721b5099394ca1be89302c24dd0d50c59a
KokuboHayate <k024g1005@g.neec.ac.jp>
2025-05-23 14:44:04 +0900

    クラスの生成

--- main.cpp
+++ main.cpp
@@ -1,28 +1,6 @@
 #include <stdio.h>
+#include "Comparison.h"
 
-
-template <typename T1, typename T2>
-class Comparison
-{
-public:
-	//メンバ変数
-	T1 numA_;
-	T2 numB_;
-
-	//コンストラクタ
-	Comparison(T1 numA_, T2 numB_) : numA_(numA_), numB_(numB_) {}
-
-	//メンバ関数
-	T1 Min() {
-		//値の低いほうを返す
-		if (numA_ < numB_) {
-			return numA_;
-		} else {
-			return static_cast<T1>(numB_);
-		}
-		return numA_;
-	}
-};
 
 int main() 
 {
