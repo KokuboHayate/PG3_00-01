1795b0fbdedca33dc8983d49169958337c898d2b
KokuboHayate <k024g1005@g.neec.ac.jp>
2025-08-01 16:43:38 +0900

    ごみ

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <chrono>
 
 int main() 
 {
-	
+	std::string a(1000000, 'a');
+
+	std::chrono::system_clock::time_point startl = std::chrono::system_clock::new();
+
 	return 0;
 }
